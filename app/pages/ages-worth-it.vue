7ab57d4073d1b8ca43e75e047b60852e5908145d
David Hafner <david.hafner@tingo.ch>
2026-03-21 17:54:14 +0100

    switch to bar chart
    Signed-off-by: David Hafner <david.hafner@tingo.ch>

--- app/pages/ages-worth-it.vue
+++ app/pages/ages-worth-it.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="h-full overflow-hidden" ref="parent">
-    <LineChart
+    <BarChart
       :data="data"
       :categories="categories"
       :height="parentHeight"
       :xFormatter="xFormatter"
+      :yFormatter="yFormatter"
       xLabel="Alter"
       yLabel="Durchschnittswert"
       class="h-full p-4"
+      :yAxis="['worthIt']"
+      :valueLabel="valueLabel"
+      xAxis="age"
+      :xNumTicks="5"
     />
   </div>
 </template>
@@ -15,6 +20,7 @@
 definePageMeta({
   name: "Lohnt es sich (Meinung nach alter)",
 });
+import type { ValueLabel } from "~~/node_modules/vue-chrts/dist/components/BarChart/BarChart.d.ts";
 import responseParser from "../helpers/responseParser";
 import type { RawResponse, Response } from "../types/response.type";
 
@@ -26,52 +32,61 @@ const parentHeight = computed(() => {
 const responses = ref<Response[]>([]);
 const data = ref<{ worthIt: number; age: string; count: number }[]>([]);
 
+const valueLabel = ref<ValueLabel>({
+  label: (d: { x: number; y: number }, index: number) => {
+    return d.y.toFixed(2);
+  },
+  backgroundColor: "var(--color-base-100)",
+  color: "var(--color-base-content)",
+  labelFontSize: 16,
+  labelSpacing: 0.015,
+});
+
 const categories = {
   worthIt: {
     name: "Sind Sie persönlich der Meinung, dass es sich lohnt zu heiraten?",
     color: "#3b82f6",
   },
-  count: {
-    name: "Anzahl Antworten",
-    color: "#10b981",
-  },
+  //   count: {
+  //     name: "Anzahl Antworten",
+  //     color: "#10b981",
+  //   },
 };
 
 const xFormatter = (i: number) => data.value[i]?.age ?? "-";
+const yFormatter = (i: number) => i.toString() ?? "-";
 
 import rawData from "~/assets/data.json";
-onMounted(() => {
-  rawData.forEach((raw) => {
-    responses.value.push(responseParser(raw as RawResponse));
-  });
-  let ageResponses = {
-    "unter 18": [] as number[],
-    "18-24 Jahre": [] as number[],
-    "25-34 Jahre": [] as number[],
-    "35-44 Jahre": [] as number[],
-    "45-54 Jahre": [] as number[],
-    "55-64 Jahre": [] as number[],
-    "65-74 Jahre": [] as number[],
-    "75 Jahre und älter": [] as number[],
-  };
-  responses.value.forEach((entry) => {
-    console.log(entry.age, entry.worthIt);
-    ageResponses[entry.age].push(entry.worthIt);
-  });
-  for (const key in ageResponses) {
-    if (ageResponses[key as keyof typeof ageResponses].length > 0) {
-      data.value.push({
-        age: key,
-        worthIt:
-          ageResponses[key as keyof typeof ageResponses].reduce(
-            (a, b) => a + b,
-            0,
-          ) / ageResponses[key as keyof typeof ageResponses].length,
-        count: ageResponses[key as keyof typeof ageResponses].length,
-      });
-    }
-  }
+rawData.forEach((raw) => {
+  responses.value.push(responseParser(raw as RawResponse));
 });
+let ageResponses = {
+  "unter 18": [] as number[],
+  "18-24 Jahre": [] as number[],
+  "25-34 Jahre": [] as number[],
+  "35-44 Jahre": [] as number[],
+  "45-54 Jahre": [] as number[],
+  "55-64 Jahre": [] as number[],
+  "65-74 Jahre": [] as number[],
+  "75 Jahre und älter": [] as number[],
+};
+responses.value.forEach((entry) => {
+  console.log(entry.age, entry.worthIt);
+  ageResponses[entry.age].push(entry.worthIt);
+});
+for (const key in ageResponses) {
+  if (ageResponses[key as keyof typeof ageResponses].length > 0) {
+    data.value.push({
+      age: key,
+      worthIt:
+        ageResponses[key as keyof typeof ageResponses].reduce(
+          (a, b) => a + b,
+          0,
+        ) / ageResponses[key as keyof typeof ageResponses].length,
+      count: ageResponses[key as keyof typeof ageResponses].length,
+    });
+  }
+}
 </script>
 
 <style>
